0ce26d423315c084ccdd736746f97abb4ed4e256
Jason.Wu <wujpkimo@gmail.com>
2021-07-30 09:46:03 +0800

    2021-07-23 Jason add demo18

--- main18.py
+++ main18.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import font as font
 from tkinter.font import Font
 from pprint import pprint
+from PIL import Image, ImageTk
 import cv2
 
 print(cv2.__version__)
@@ -14,7 +15,13 @@ cap = cv2.VideoCapture(0)
 
 
 def show_frames():
-    cv2Image = cv2.cvtColor(cap.read())
+    cv2Image = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(cv2Image)
+    imageTk = ImageTk.PhotoImage(image=img)
+    label1.imgtk = imageTk
+    label1.configure(image=imageTk)
+    label1.after(20, show_frames())
+    pass
 
 
 show_frames()
